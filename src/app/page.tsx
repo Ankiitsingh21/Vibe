46b039483fd5a3a3f87ad328fb77b6aa16954e54
ankit singh <ankiitsingh21@gmail.com>
2025-08-19 03:16:59 +0530

    Basic setup done

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import {Button} from "../../src/components/ui/button";
+const Page = () => {
+  return ( 
+    <div>
+      <Button variant={"new"} >
+        Click Me
+      </Button>
+    </div>
+   );
+}
+ 
+export default Page;
