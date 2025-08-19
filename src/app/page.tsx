b33aab675855f2ebb267fbdd89e500178c477f20
ankit singh <ankiitsingh21@gmail.com>
2025-08-19 17:41:12 +0530

    prefetching on the server side with trpc trpc setup trpc usage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
-import {Button} from "../../src/components/ui/button";
-import { prisma } from "@/lib/db";
-const Page = async () => {
-  const posts = await prisma.post.findMany();
+import { caller, getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import Client from "./client";
+import { Suspense } from "react";
+
+const Page =  async() => {
+  // console.log("SERVER COMPONENT");
+  // const data = await caller.hello({text:"Ankit singh"});
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.hello.queryOptions({text:"ankit singh"}));
   return ( 
-    <div>
-      {JSON.stringify(posts,null,2)}
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading..</p>} >
+        <Client/>
+      </Suspense>
+    </HydrationBoundary>
    );
 }
  
