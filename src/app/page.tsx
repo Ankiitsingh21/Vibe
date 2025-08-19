a1ce0be1476bc2d28afd49eb601cf2dca5d54e16
ankit singh <ankiitsingh21@gmail.com>
2025-08-19 21:23:00 +0530

    background job is done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,17 @@
-import { caller, getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import Client from "./client";
-import { Suspense } from "react";
+"use client"
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
 
-const Page =  async() => {
-  // console.log("SERVER COMPONENT");
-  // const data = await caller.hello({text:"Ankit singh"});
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.hello.queryOptions({text:"ankit singh"}));
+const Page =  () => {
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({}));
   return ( 
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<p>Loading..</p>} >
-        <Client/>
-      </Suspense>
-    </HydrationBoundary>
+    <div className="p-4 max-w-7xl mx-auto" >
+      <Button  onClick={()=>invoke.mutate({text:"alice"})} >
+        Invoke Background Job
+      </Button>
+    </div>
    );
 }
  
