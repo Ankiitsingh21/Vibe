0d38f0ccc23ade466575082dd1637359f85ae321
ankit singh <ankiitsingh21@gmail.com>
2025-08-20 06:16:22 +0530

    added api for gemini

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,24 @@
+import { openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "",
+      model: openai({
+        model: "gemini-1.5-flash-8b", // ✅ Free-tier Gemini model
+        apiKey: process.env.GEMINI_API_KEY, // store in .env
+        baseUrl: "https://generativelanguage.googleapis.com/v1beta/openai/", 
+      }),
+    });
+
+    const { output } = await codeAgent.run(
+      `${event.data.value}`
+    );
+
+    return { output };
+  }
 );
