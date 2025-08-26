f69390ece63750afada3955d379620f5868b9e00
Ankit Singh <ankiitsingh21@gmail.com>
2025-08-26 13:46:38 +0530

    e2b sandboc setup completed

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,17 +1,25 @@
+import {Sandbox} from "@e2b/code-interpreter";
 import { openai, createAgent } from "@inngest/agent-kit";
+
+
 import { inngest } from "./client";
+import { getSandBox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id",async()=>{
+      const sandbox =await Sandbox.create("vibe-nextjs-test-2025");
+      return sandbox.sandboxId;
+    })
     const codeAgent = createAgent({
       name: "code-agent",
       system: "",
       model: openai({
-        model: "gemini-1.5-flash-8b", // ✅ Free-tier Gemini model
-        apiKey: process.env.GEMINI_API_KEY, // store in .env
-        baseUrl: process.env.base_url, 
+        model: "gemini-2.5-flash",  // Gemini model name
+        apiKey: process.env.GEMINI_API_KEY, 
+        baseUrl: "https://generativelanguage.googleapis.com/v1beta/openai/", // Gemini's OpenAI-compatible endpoint
       }),
     });
 
@@ -19,6 +27,15 @@ export const helloWorld = inngest.createFunction(
       `${event.data.value}`
     );
 
-    return { output };
+    const sendboxUrl = await step.run(
+      "get-sandbox-url",
+      async () => {
+        const sandbox = await getSandBox(sandboxId);
+        const host =  sandbox.getHost(3000);
+        return `https://${host}`;
+      }
+    )
+
+    return { output  , sendboxUrl};
   }
 );
