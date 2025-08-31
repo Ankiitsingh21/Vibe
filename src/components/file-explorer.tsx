36012d6c47ac748671a40dc2b7450add5c91914d
Ankit Singh <ankiitsingh21@gmail.com>
2025-09-01 01:02:44 +0530

    file explorer completed

--- src/components/file-explorer.tsx
+++ src/components/file-explorer.tsx
@@ -29,6 +29,70 @@ function getLanguageFromExtension(filename:string):string{
         return extension || "text"
 }
 
+interface FileBreadCrumbsProps{
+        filePath:string
+}
+
+const FileBreadcrumb = ({ filePath }: FileBreadCrumbsProps) => {
+  const pathSegments = filePath.split("/");
+  const maxSegments = 4;
+
+  const renderBreadcrumbItems = () => {
+    if (pathSegments.length <= maxSegments) {
+      // Show all segments if 4 or less
+      return pathSegments.map((segment, index) => {
+        const isLast = index === pathSegments.length - 1;
+        return (
+          <Fragment key={index} >
+                <BreadcrumbItem>
+                 {isLast ? (
+                        <BreadcrumbPage className="font-medium" >
+                         {segment}
+                        </BreadcrumbPage>
+                 ):(
+                        <span className="text-muted-foreground" >
+                                {segment}
+                        </span>
+                 )}
+                </BreadcrumbItem>
+                {!isLast && <BreadcrumbSeparator/>}
+          </Fragment>
+        )
+      })
+    }
+  else{
+      const firstSegment = pathSegments[0];
+      const lastSegment = pathSegments[pathSegments.length-1]
+        return (
+                <>
+                
+                   <BreadcrumbItem>
+                   <span className="text-muted-foreground">
+                     {firstSegment}
+                   </span>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                      <BreadcrumbEllipsis />
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                      <BreadcrumbPage className="font-medium">
+                        {lastSegment}
+                      </BreadcrumbPage>
+                    </BreadcrumbItem>
+                 </>
+        )
+  }}
+  return (
+        <Breadcrumb>
+         <BreadcrumbList>
+           {renderBreadcrumbItems()}
+         </BreadcrumbList>
+        </Breadcrumb>
+  )
+}
+
 interface Props{
         files: FileCollection
 }
@@ -36,6 +100,7 @@ interface Props{
 export const FileExproper=({
         files
 }:Props)=>{
+        const [copy,setCopy] = useState(false);
         const [selectedFile , setSelectedFile] = useState<string | null>(()=>{
                 const fileKeys = Object.keys(files);
                 return fileKeys.length > 0 ? fileKeys[0] : null
@@ -50,6 +115,15 @@ export const FileExproper=({
                         setSelectedFile(filePath)
                 }
         },[files])
+        const handleCopy = useCallback(()=>{
+                if(selectedFile){
+                        navigator.clipboard.writeText(files[selectedFile]);
+                        setCopy(true);
+                        setTimeout(()=>{
+                                setCopy(false)
+                        },2000)
+                }
+        },[selectedFile,files])
         return (
                 <ResizablePanelGroup direction="horizontal" >
                         <ResizablePanel defaultSize={30} minSize={30} className="bg-sidebar" >
@@ -60,9 +134,10 @@ export const FileExproper=({
                                 {selectedFile && files[selectedFile]?(
                                         <div className="h-full w-full flex flex-col" >
                                                 <div className="border-b bg-sidebar px-4 py-2 flex justify-between items-center gap-x-2" >
+                                                        <FileBreadcrumb filePath={selectedFile}/>
                                                         <Hint text="Copy to Clipboard" side="bottom" >
-                                                                <Button variant={"outline"} size={"icon"} className="ml-auto" onClick={()=>{}} disabled={false} >
-                                                                        <CopyIcon/>
+                                                                <Button variant={"outline"} size={"icon"} className="ml-auto" onClick={handleCopy} disabled={copy} >
+                                                                       {copy ? <CopyCheckIcon/>: <CopyIcon/>}
                                                                 </Button>
                                                         </Hint>
 
